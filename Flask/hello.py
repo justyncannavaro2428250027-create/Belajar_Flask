8f98629b5da6780d07490478e7b3f6992e32d785
GoDisCover <jessenco122@gmail.com>
2025-11-18 12:54:22 +0700

    post

--- Flask/hello.py
+++ Flask/hello.py
@@ -1,6 +1,10 @@
 # import flash module
+import email
 from turtle import title
+from urllib import request
 from flask import Flask, render_template
+from flask import request
+
 # create an instance of the Flask class
 app = Flask(__name__, template_folder='views')
 # define a route for the root URL
@@ -14,8 +18,17 @@ def about():
     title = "About Page"
     return render_template('about.html', title = title)
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        #Proses data form di sini
+        nama = request.form['nama']
+        email = request.form['email']
+        pesan = request.form['pesan']
+
+        #Tampilan pada terminal
+        print(f"Nama: {nama}, Email: {email}, Pesan: {pesan}")
+        
     title = "Contact Page"
     return render_template('contact.html', title = title)
 
